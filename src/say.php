5069e023de54554d593b2a0c2c9a45005f853022
xy <782043356@qq.com>
2020-03-27 14:24:59 +0800

    first commit

---
+++ src/say.php
@@ -0,0 +1,15 @@
+<?php
+namespace xytool\sayHello;
+class say{
+    /**
+     * @Notes:
+     * @Interface index
+     * @return string
+     * @author: xuyong
+     * @Time: 2020/3/27   1:59 下午
+     */
+    static function index()
+    {
+        return 'hello xuyong';
+    }
+}
